486c88abd771cf3381a0bb5e4ae3ddcdae1b9d71
Next <next@next-hyd-561.nexteducation.com>
2016-11-17 18:17:29 +0530

    updated

--- controller/readCtrl.js
+++ controller/readCtrl.js
@@ -26,33 +26,35 @@ angular.module('MyApp').controller('readCtrl', function ($scope) {
 
     // Display custom canvas.
     // In this case it's a blue, 5 pixel border that 
-    // resizes along with the browser window.					
+    // resizes along with the browser window.	
+     var jarra=[];
+        var jarra1=[];				
     function mobileView() {
-        ctx.strokeStyle = 'blue';
-        ctx.lineWidth = '5';
-        ctx.strokeRect(0, 0, window.innerWidth, window.innerHeight);
+        ctx.strokeStyle = 'green';
+        ctx.lineWidth = '3';
+        // ctx.strokeRect(0, 0, window.innerWidth, window.innerHeight);
 
         var a = 20, a1 = 140;
         var b = 15, b1 = 60;
         var xx = 200, xx1 = 180;
         var xy = 15, xy1 = 60;
+       
         $.getJSON('data.json', function (data11) {
 
             $.each(data11, function (index, element) {
                 $.each(element, function (index, element) {
                     var imageObj1 = new Image();
+
                     imageObj1.onload = function () {
-
-
                         ctx.drawImage(imageObj1, a, b, canvas.width - 200, canvas.height - 350);
-                        a = a + 1;
-                        b = b + 80;
+                      
+                        b = b + 100;
                         ctx.beginPath();
-                        ctx.arc(a1, b1, 10, 0, 2 * Math.PI, canvas.width - 200, canvas.height - 350);
+                        ctx.arc(a1, b1, 5, 0, 2 * Math.PI);
                         ctx.stroke();
-                        a1 = a1 + 1;
-                        b1 = b1 + 70;
-
+                         jarra.push({x:a1,y:b1,id:element.id})
+                        b1 = b1 + 100;
+                       
 
                     };
                     imageObj1.id = element.id;
@@ -73,12 +75,13 @@ angular.module('MyApp').controller('readCtrl', function ($scope) {
                     var imageObj12 = new Image();
                     imageObj12.onload = function () {
                         ctx.drawImage(imageObj12, xx, xy, canvas.width - 200, canvas.height - 350);
-                        xy = xy + 80;
+                        xy = xy + 100;
                         ctx.beginPath();
-                        ctx.arc(xx1, xy1, 10, 0, 2 * Math.PI, canvas.width - 200, canvas.height - 350);
+                        ctx.arc(xx1, xy1, 5, 0, 2 * Math.PI);
                         ctx.stroke();
-                        xx1 = xx1 + 1;
-                        xy1 = xy1 + 70
+                      jarra1.push({x:xx1,y:xy1,id:element.id})
+                        xy1 = xy1 + 100
+                          
                     };
                     imageObj12.id = element.id;
                     imageObj12.src = element.Image;
@@ -97,9 +100,9 @@ angular.module('MyApp').controller('readCtrl', function ($scope) {
 
 
 
-        ctx.strokeStyle = 'blue';
-        ctx.lineWidth = '5';
-        ctx.strokeRect(0, 0, window.innerWidth, window.innerHeight);
+        ctx.strokeStyle = 'green';
+        ctx.lineWidth = '3';
+        // ctx.strokeRect(0, 0, window.innerWidth, window.innerHeight);
 
         var a = 50, a1 = 230;
         var b = 40, b1 = 70;
@@ -114,13 +117,13 @@ angular.module('MyApp').controller('readCtrl', function ($scope) {
 
 
                         ctx.drawImage(imageObj1, a, b, canvas.width - 900, canvas.height - 500);
-                        a = a + 5;
-                        b = b + 150;
+                      
+                        b = b + 100;
                         ctx.beginPath();
                         ctx.arc(a1, b1, 10, 0, 2 * Math.PI, canvas.width - 900, canvas.height - 500);
                         ctx.stroke();
-                        a1 = a1 + 1;
-                        b1 = b1 + 150;
+                       jarra.push({x:a1,y:b1,id:element.id})
+                        b1 = b1 + 100;
 
 
                     };
@@ -140,12 +143,12 @@ angular.module('MyApp').controller('readCtrl', function ($scope) {
                     var imageObj12 = new Image();
                     imageObj12.onload = function () {
                         ctx.drawImage(imageObj12, xx, xy, canvas.width - 900, canvas.height - 500);
-                        xy = xy + 150;
+                        xy = xy + 100;
                         ctx.beginPath();
                         ctx.arc(xx1, xy1, 10, 0, 2 * Math.PI, canvas.width - 900, canvas.height - 500);
                         ctx.stroke();
-                        xx1 = xx1 + 1;
-                        xy1 = xy1 + 150
+                        jarra1.push({x:xx1,y:xy1,id:element.id})
+                        xy1 = xy1 + 100
                     };
                     imageObj12.id = element.id;
                     imageObj12.src = element.Image;
@@ -159,35 +162,34 @@ angular.module('MyApp').controller('readCtrl', function ($scope) {
 
 
     }
-  //canvas for deskView
+    //canvas for deskView
     function deskView() {
 
 
+        ctx.strokeStyle = 'green';
+        ctx.lineWidth = '3';
+        // ctx.strokeRect(0, 0, window.innerWidth, window.innerHeight);
 
-        ctx.strokeStyle = 'blue';
-        ctx.lineWidth = '5';
-        ctx.strokeRect(0, 0, window.innerWidth, window.innerHeight);
-
-        var a = 90, a1 = 520;
+        var a = 90, a1 = 320;
         var b = 40, b1 = 125;
-        var xx = 1500, xx1 = 1200;
+        var xx = 1000, xx1 = 900;
         var xy = 40, xy1 = 125;
         $.getJSON('data.json', function (data11) {
 
             $.each(data11, function (index, element) {
                 $.each(element, function (index, element) {
                     var imageObj1 = new Image();
-                  
+
                     imageObj1.onload = function () {
 
 
                         ctx.drawImage(imageObj1, a, b);
-                        a = a + 5;
+                       
                         b = b + 150;
                         ctx.beginPath();
                         ctx.arc(a1, b1, 10, 0, 2 * Math.PI);
                         ctx.stroke();
-                        a1 = a1 + 1;
+                        jarra.push({x:a1,y:b1,id:element.id})
                         b1 = b1 + 150;
 
 
@@ -211,7 +213,7 @@ angular.module('MyApp').controller('readCtrl', function ($scope) {
                         ctx.beginPath();
                         ctx.arc(xx1, xy1, 10, 0, 2 * Math.PI);
                         ctx.stroke();
-                        xx1 = xx1 + 1;
+                        jarra1.push({x:xx1,y:xy1,id:element.id})
                         xy1 = xy1 + 150
                     };
                     imageObj12.id = element.id;
@@ -226,22 +228,103 @@ angular.module('MyApp').controller('readCtrl', function ($scope) {
 
 
 
+
+    }
+
+
+
+    function otherView() {
+        ctx.strokeStyle = 'green';
+        ctx.lineWidth = '3';
+        // ctx.strokeRect(0, 0, window.innerWidth, window.innerHeight);
+
+        var a = 10, a1 = 120;
+        var b = 20, b1 = 60;
+        var xx = 250, xx1 = 200;
+        var xy = 20, xy1 = 60;
+        $.getJSON('data.json', function (data11) {
+
+            $.each(data11, function (index, element) {
+                $.each(element, function (index, element) {
+                    var imageObj1 = new Image();
+
+                    imageObj1.onload = function () {
+                        ctx.drawImage(imageObj1, a, b, 60, 60);
+                      
+                        b = b + 100;
+                        ctx.beginPath();
+                        ctx.arc(a1, b1, 5, 0, 2 * Math.PI);
+                        ctx.stroke();
+                        jarra.push({x:a1,y:b1,id:element.id})
+                        b1 = b1 + 100;
+                         
+
+                    };
+                    imageObj1.id = element.id;
+                    imageObj1.src = element.Image
+                    console.log(imageObj1.id)
+
+
+                })
+            })
+        })
+
+        $.getJSON('data1.json', function (data11) {
+
+            $.each(data11, function (index, element) {
+                $.each(element, function (index, element) {
+                    var imageObj12 = new Image();
+                    imageObj12.onload = function () {
+                        ctx.drawImage(imageObj12, xx, xy, 60, 60);
+                        xy = xy + 100;
+                        ctx.beginPath();
+                        ctx.arc(xx1, xy1, 5, 0, 2 * Math.PI);
+                        ctx.stroke();
+                        jarra1.push({x:xx1,y:xy1,id:element.id})
+                         xy1 = xy1 + 100
+                         
+                    };
+                    imageObj12.id = element.id;
+                    imageObj12.src = element.Image;
+
+
+
+
+                })
+            })
+        })
+
+
     }
 
     var x, y;
     var co_Coordinates = [];
     canvas.addEventListener('click', function (evt) {
+   
+        x = evt.layerX-canvas.offsetLeft;
+        y = evt.layerY-canvas.offsetTop;
+        // var hashkey = y.toString();
+        // var po = hashkey.substr(0, 2)
+        console.log(x,y,jarra,jarra1)
 
-        x = evt.layerX;
-        y = evt.layerY;
-        var hashkey = y.toString();
-        var po = hashkey.substr(0, 2)
-        console.log(po)
 
-        co_Coordinates.push({ x: x, y: y, po: po })
-        console.log(co_Coordinates)
-        if (co_Coordinates.length == 2) {
-            if (co_Coordinates[0].po == 12 && co_Coordinates[1].po == 12 || co_Coordinates[0].po == 25 && co_Coordinates[1].po == 40 || co_Coordinates[0].po == 36 && co_Coordinates[1].po == 25 || co_Coordinates[0].po == 50 && co_Coordinates[1].po == 12) {
+        for(var i=0;i<jarra.length;i++){
+            if((x >= (jarra[i].x -10) && x <= (jarra[i].x + 10))&& (y >= (jarra[i].y - 10) && y <= (jarra[i].y + 10)))
+            {
+                co_Coordinates.push({ x: x, y: y,id:jarra[i].id })
+                console.log(co_Coordinates)
+            }
+        }
+        for(var i=0;i<jarra1.length;i++){
+            if((x >= (jarra1[i].x -10) && x <= (jarra1[i].x + 10))&& (y >= (jarra1[i].y - 10) && y <= (jarra1[i].y + 10)))
+            {
+                co_Coordinates.push({ x: x, y: y,id:jarra[i].id })
+                console.log(co_Coordinates)
+            }
+        }
+         if (co_Coordinates.length == 2) {
+            if (co_Coordinates[0].id==co_Coordinates[1].id) 
+            {
                 drawLine(co_Coordinates);
             }
             else {
@@ -251,6 +334,10 @@ angular.module('MyApp').controller('readCtrl', function ($scope) {
 
 
         }
+           
+            
+
+        
 
 
 
@@ -259,6 +346,7 @@ angular.module('MyApp').controller('readCtrl', function ($scope) {
     })
 
     function drawLine(co_Coordinates) {
+       
         drawNextLine(ctx, co_Coordinates[0].x, co_Coordinates[0].y, co_Coordinates[1].x, co_Coordinates[1].y);
     }
     function drawNextLine(ctx, x, y, x1, y1) {
@@ -291,6 +379,7 @@ angular.module('MyApp').controller('readCtrl', function ($scope) {
         ctx.lineTo(x1, y1);
         if (close_it) ctx.closePath();
         ctx.stroke();
+         co_Coordinates = [];
 
 
 
@@ -318,6 +407,10 @@ angular.module('MyApp').controller('readCtrl', function ($scope) {
         else if (canvas.width >= 980 && canvas.height >= 570) {
             console.log('other')
             deskView();
+        }
+        else {
+
+            otherView();
         }
 
     }
